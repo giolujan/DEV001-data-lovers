a5f2b0050493e75ca21b1607e581c0821191e055
ann-dy2712 <113961615+ann-dy2712@users.noreply.github.com>
2022-10-31 15:17:16 -0600

    Mostrar y boton Search

--- src/data.js
+++ src/data.js
@@ -1,9 +1,25 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+//export const example = () => {
+  //return 'example';
+//};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+
+//export const anotherExample = () => {
+  //return 'OMG';
+//};
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// Trae Films
+export const getFilm = (data) => {
+  const dataGhibli = data.films.map((arr) => arr);
+  return dataGhibli;
+}
+ console.log (getFilm);
+ //Searcher all films
+ export const searchFilms = (data, condition, value) => {
+  return data.filter(item => item[condition].toLowerCase().includes(value.toLowerCase()));
+}
+console.log(searchFilms);
+
+/////////////////////////////////////////////////////////////////////////////////////////////
